d8a1647db2d405a66a7b814d31b9e4f10f7a52d6
Monish <s.monish2006@gmail.com>
2021-06-07 21:24:11 -0400

    fixed Drops.js class

--- Drops.js
+++ Drops.js
@@ -14,12 +14,11 @@ class Drops {
         ellipse(this.body.position.x, this.body.position.y)
         pop();
     }
-}
-
-    function update() {
+    update() {
         if(this.drops.position.y > height) {
             Matter.Body.setPosition(this.drops, {x: random(0, 400), y: random(0, 400)})
         }
-    }       
+    }
+}       
 
 
