bb7de5964f86ce95d457d03b20ce10f64b9320d5
Monish <s.monish2006@gmail.com>
2021-06-13 10:29:57 -0400

    finished Project 41

--- Drops.js
+++ Drops.js
@@ -10,13 +10,13 @@ class Drops {
     display() {
         push();
         ellipseMode(CENTER);
-        fill("red");
-        ellipse(this.body.position.x, this.body.position.y)
+        fill("blue");
+        ellipse(this.body.position.x, this.body.position.y, this.radius, this.radius)
         pop();
     }
     update() {
-        if(this.drops.position.y > height) {
-            Matter.Body.setPosition(this.drops, {x: random(0, 400), y: random(0, 400)})
+        if(this.body.position.y > height) {
+            Matter.Body.setPosition(this.body, {x: random(0, 400), y: random(0, 400)})
         }
     }
 }       
--- sketch.js
+++ sketch.js
@@ -8,24 +8,35 @@ var maxDrops = 100;
 var drops = []
 
 function preload() {
-    
+    boyImage = loadImage("boy.png");
 }
 
 function setup() {
-   engine= Engine.create() 
-   world= engine.world
+   engine = Engine.create() 
+   world = engine.world
    
    createCanvas(400, 800);
 
    umbrella = new Umbrella(200, 20);
 
+   if (frameCount % 100 == 0) {
    for(var i = 0; i < maxDrops; i++) {
       drops.push(new Drops(random(0, 400), random(0, 400)));
    }
 }
+}
 
 function draw() {
-   update();
+   background("gray");
+
+   Engine.update(engine);
+   
+   for(var j = 0; j < drops.length; j++) {
+      drops[j].update();
+      drops[j].display();
+   }
+
+   image(boyImage, 200, 700, 100, 100);
 
    drawSprites();
 }   
